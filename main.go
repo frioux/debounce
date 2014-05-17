9899e4355dd5742315e02d80828310d412c99e45
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-17 15:24:20 -0500

    format

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-   "bufio"
-   "fmt"
-   "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func cat(c chan string, e chan error, quit chan struct{}) {
-   scanner := bufio.NewScanner(os.Stdin)
-   for scanner.Scan() {
-      c <- scanner.Text()
-   }
-   if err := scanner.Err(); err != nil {
-      e <- err
-   }
-   quit <- struct{}{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		c <- scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		e <- err
+	}
+	quit <- struct{}{}
 }
 
 func main() {
-   c := make(chan string)
-   quit := make(chan struct{})
-   err := make(chan error)
-   go cat(c, err, quit)
-   for {
-      select {
-         case x := <-c:
-            fmt.Println(x)
-         case x := <-err:
-            fmt.Fprintln(os.Stderr, "reading standard input:", x)
-         case <-quit:
-            return
-      }
-   }
+	c := make(chan string)
+	quit := make(chan struct{})
+	err := make(chan error)
+	go cat(c, err, quit)
+	for {
+		select {
+		case x := <-c:
+			fmt.Println(x)
+		case x := <-err:
+			fmt.Fprintln(os.Stderr, "reading standard input:", x)
+		case <-quit:
+			return
+		}
+	}
 }
