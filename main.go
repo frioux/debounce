115e50150b0a91aa92df76ddbcd89fd2d6ea29b3
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-17 15:17:12 -0500

    convert cat to a channel

--- main.go
+++ main.go
@@ -6,12 +6,30 @@ import (
    "os"
 )
 
-func main() {
+func cat(c chan string, e chan error, quit chan struct{}) {
    scanner := bufio.NewScanner(os.Stdin)
    for scanner.Scan() {
-      fmt.Println(scanner.Text())
+      c <- scanner.Text()
    }
    if err := scanner.Err(); err != nil {
-      fmt.Fprintln(os.Stderr, "reading standard input:", err)
+      e <- err
+   }
+   quit <- struct{}{}
+}
+
+func main() {
+   c := make(chan string)
+   quit := make(chan struct{})
+   err := make(chan error)
+   go cat(c, err, quit)
+   for {
+      select {
+         case x := <-c:
+            fmt.Println(x)
+         case x := <-err:
+            fmt.Fprintln(os.Stderr, "reading standard input:", x)
+         case <-quit:
+            return
+      }
    }
 }
