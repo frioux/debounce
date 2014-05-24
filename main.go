d4e98c5ab841c966821edb08f63193c9a32daafb
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-24 10:41:31 -0500

    allow triggering begin or end edge

--- main.go
+++ main.go
@@ -21,32 +21,44 @@ func cat(c chan string, e chan error, quit chan struct{}) {
 
 func main() {
 	var timeoutSeconds = flag.Int("timeout", 1, "amount of time between output")
+	var begin = flag.Bool("beginEdge", false, "trigger at start")
+	var end = flag.Bool("endEdge", true, "trigger at end")
 	flag.Parse()
+
 	c := make(chan string)
 	quit := make(chan struct{})
 	err := make(chan error)
+
 	go cat(c, err, quit)
+
 	for {
 		var x string
 		select {
 		case x = <-c:
+			if *begin {
+				fmt.Println(x)
+			}
 		case x := <-err:
 			fmt.Fprintln(os.Stderr, "reading standard input:", x)
 		case <-quit:
 			return
 		}
 		timeout := time.After(time.Duration(*timeoutSeconds) * time.Second)
+		shouldPrint := false
 	InnerLoop:
 		for {
 			select {
 			case x = <-c:
+				shouldPrint = true
 				timeout = time.After(time.Duration(*timeoutSeconds) * time.Second)
 			case x := <-err:
 				fmt.Fprintln(os.Stderr, "reading standard input:", x)
 			case <-quit:
 				return
 			case <-timeout:
-				fmt.Println(x)
+				if *end && shouldPrint {
+					fmt.Println(x)
+				}
 				break InnerLoop
 			}
 		}
