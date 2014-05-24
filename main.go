711b165fcbf586e67ed0118697d6e57fb5eccb57
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-24 08:29:28 -0500

    make debounce work for realz

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"time"
 )
 
 func cat(c chan string, e chan error, quit chan struct{}) {
@@ -30,6 +31,20 @@ func main() {
 			fmt.Fprintln(os.Stderr, "reading standard input:", x)
 		case <-quit:
 			return
+		}
+		timeout := time.After(2 * time.Second)
+	InnerLoop:
+		for {
+			select {
+			case <-c:
+				timeout = time.After(2 * time.Second)
+			case x := <-err:
+				fmt.Fprintln(os.Stderr, "reading standard input:", x)
+			case <-quit:
+				return
+			case <-timeout:
+				break InnerLoop
+			}
 		}
 	}
 }
