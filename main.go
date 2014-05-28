28474378139c48d7f9a25e447f2df8ca980eaf67
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-27 20:03:01 -0500

    add help

--- main.go
+++ main.go
@@ -21,13 +21,27 @@ func cat(c chan string, e chan error, quit chan struct{}) {
 
 func main() {
 	var timeoutSeconds float64
-	var begin, end bool
+	var begin, end, h, help bool
 
 	flag.Float64Var(&timeoutSeconds, "timeout", 1, "amount of time between output")
 	flag.BoolVar(&begin, "beginEdge", false, "trigger at start")
 	flag.BoolVar(&end, "endEdge", true, "trigger at end")
+	flag.BoolVar(&h, "h", false, "help for debounce")
+	flag.BoolVar(&help, "help", false, "help for debounce")
 
 	flag.Parse()
+
+	if h || help {
+		fmt.Println("\n" +
+			" debounce          [--beginEdge] [--endEdge] [--timeout 2] [-h|--help]\n" +
+			"    --beginEdge     pass this flag to output at the start of a cycle\n" +
+			"    --endEdge       pass this flag to output after the end of a cycle\n" +
+			"    --timeout       set the timeout in seconds, default is 1 second\n" +
+			"\n" +
+			"    -h --help       print usage message and exit\n",
+		)
+		return
+	}
 
 	c := make(chan string)
 	quit := make(chan struct{})
