ee8f61ca3d2f93fd33aeaf5d9825257f7eacbb01
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-27 19:12:18 -0500

    remove pointers

--- main.go
+++ main.go
@@ -20,9 +20,13 @@ func cat(c chan string, e chan error, quit chan struct{}) {
 }
 
 func main() {
-	var timeoutSeconds = flag.Float64("timeout", 1, "amount of time between output")
-	var begin = flag.Bool("beginEdge", false, "trigger at start")
-	var end = flag.Bool("endEdge", true, "trigger at end")
+	var timeoutSeconds float64
+	var begin, end bool
+
+	flag.Float64Var(&timeoutSeconds, "timeout", 1, "amount of time between output")
+	flag.BoolVar(&begin, "beginEdge", false, "trigger at start")
+	flag.BoolVar(&end, "endEdge", true, "trigger at end")
+
 	flag.Parse()
 
 	c := make(chan string)
@@ -35,7 +39,7 @@ func main() {
 		var x string
 		select {
 		case x = <-c:
-			if *begin {
+			if begin {
 				fmt.Println(x)
 			}
 		case x := <-err:
@@ -43,20 +47,20 @@ func main() {
 		case <-quit:
 			return
 		}
-		timeout := time.After(time.Duration(*timeoutSeconds) * time.Second)
+		timeout := time.After(time.Duration(timeoutSeconds) * time.Second)
 		shouldPrint := false
 	InnerLoop:
 		for {
 			select {
 			case x = <-c:
 				shouldPrint = true
-				timeout = time.After(time.Duration(*timeoutSeconds) * time.Second)
+				timeout = time.After(time.Duration(timeoutSeconds) * time.Second)
 			case x := <-err:
 				fmt.Fprintln(os.Stderr, "reading standard input:", x)
 			case <-quit:
 				return
 			case <-timeout:
-				if *end && shouldPrint {
+				if end && shouldPrint {
 					fmt.Println(x)
 				}
 				break InnerLoop
